bf529d87b784eeb67491ab570322a5c4160a71ac
Elias Oltmanns <elias.oltmanns@gwdg.de>
2024-09-08 20:06:27 +0200

    Add a check module
    Perform various checks on data sets that have been converted to JSON
    for compliance with AVefi requirements. Currently, records with
    unresolvable references in slots like is_part_of, is_manifestation_of
    etc. are identified and optionally removed. Additionaly, titles are
    checked for a maximum length of 250 characters and violating records
    can optionally be removed.

---
+++ src/efi_conv/check.py
@@ -0,0 +1,105 @@
+from collections import defaultdict
+import logging
+from typing import List
+
+from avefi_schema import model as efi
+from jsonasobj2 import as_dict
+from linkml_runtime.loaders import json_loader
+
+
+log = logging.getLogger(__name__)
+
+
+def pass_checks(
+        efi_records: List[efi.MovingImageRecord],
+        remove_invalid=False) -> bool:
+    id_lookup = {}
+    dependants_by_ref = defaultdict(list)
+    all_was_fine = True
+
+    for rec in efi_records:
+        if exceeds_field_limit(rec):
+            if all_was_fine:
+                all_was_fine = False
+            if remove_invalid:
+                efi_records.remove(rec)
+                continue
+        if not rec.has_identifier:
+            raise ValueError(f"has_identifier is missing in record: {rec}")
+        record_ids = []
+        for identifier in rec.has_identifier:
+            record_id = HashableId(**as_dict(identifier))
+            if record_id in id_lookup:
+                raise ValueError(f"Identifier is not unique: {record_id}")
+            record_ids.append(record_id)
+            id_lookup[record_id] = (rec, record_ids)
+        if isinstance(rec, efi.WorkVariant):
+            link_attributes = ('is_part_of', 'is_variant_of')
+        elif isinstance(rec, efi.Manifestation):
+            # Ignore has_item here
+            link_attributes = ('is_manifestation_of', 'same_as')
+        elif isinstance(rec, efi.Item):
+            link_attributes = ('is_item_of', 'is_copy_of', 'is_derivative_of')
+        else:
+            raise ValueError(
+                f"Cannot handle {type(rec)} (record={rec})")
+        for attr_name in link_attributes:
+            attr = getattr(rec, attr_name)
+            if attr is None:
+                attr = []
+            elif not isinstance(attr, list):
+                attr = [attr]
+            for identifier in attr:
+                ref = HashableId(**as_dict(identifier))
+                dependants_by_ref[ref].append(record_id)
+
+    for ref in list(dependants_by_ref.keys()):
+        if ref not in id_lookup and ref.category == 'avefi:LocalResource':
+            log.error(f"Unresolvable reference: {ref}")
+            if all_was_fine:
+                all_was_fine = False
+            if remove_invalid:
+                purge_dependant_records(
+                    ref, efi_records, id_lookup, dependants_by_ref)
+    return all_was_fine
+
+
+def purge_dependant_records(ref, record_list, id_lookup, dependants_by_ref):
+    for record_id in dependants_by_ref[ref]:
+        try:
+            rec, ids = id_lookup[record_id]
+        except KeyError:
+            continue
+        record_list.remove(rec)
+        for record_id in ids:
+            del id_lookup[record_id]
+            purge_dependant_records(
+                record_id, record_list, id_lookup, dependants_by_ref)
+
+
+class HashableId:
+    def __init__(self, category, id):
+        self.category = category
+        self.id = id
+        self.name = f"{category}.{id}"
+
+    def __eq__(self, other):
+        return other.id == self.id and other.category == self.category
+
+    def __hash__(self):
+        return hash(self.name)
+
+    def __str__(self):
+        return self.name
+
+
+def exceeds_field_limit(efi_record):
+    titles = [efi_record.has_primary_title]
+    titles.extend(efi_record.has_alternative_title)
+    for title in titles:
+        if len(title.has_name) > 250:
+            log.error(
+                f"Record {efi_record.has_identifier[0].id} violates limit of"
+                f" 250 characters on title length: {title.has_name}")
+            return True
+    return False
