4a76ee5384070e8e8a0f706daedab9c6d1401ea9
Elias Oltmanns <elias.oltmanns@gwdg.de>
2025-04-04 19:03:51 +0200

    Add typing to load and dump functions

--- src/efi_conv/avefi.py
+++ src/efi_conv/avefi.py
@@ -1,19 +1,24 @@
+from typing import List
+
 from avefi_schema import model as efi
 from linkml_runtime import dumpers, loaders
 
 
-def load(source):
+def load(source: str) -> List[efi.MovingImageRecord]:
     """Load AVefi records from file or string."""
-    return loaders.json_loader.load_any(source, efi.MovingImageRecord)
+    result = loaders.json_loader.load_any(source, efi.MovingImageRecord)
+    if not isinstance(result, list):
+        result = [result]
+    return result
 
 
-def dump(records, to_file, **kwargs):
+def dump(records: List[efi.MovingImageRecord], to_file: str, **kwargs):
     """Dump AVefi records to JSON file."""
     if 'inject_type' not in kwargs:
         kwargs['inject_type'] = False
     dumpers.json_dumper.dump(records, to_file, **kwargs)
 
 
-def dumps(records):
+def dumps(records: List[efi.MovingImageRecord]) -> str:
     """Dump AVefi records to string (in JSON format)."""
     return dumpers.json_dumper.dumps(records, inject_type=False)
