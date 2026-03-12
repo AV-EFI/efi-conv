AV-EFI/efi-conv

commit d2aedeefe962c74f0546b9ec684fa746dc91e760

Apply ruff check autofixes

src/efi_conv/main.py
@@ -2,7 +2,6 @@ import logging
 import logging.config
 import os
 
-
 # Configure logging before importing local modules
 log = logging.getLogger(__name__)
 loglevel = os.environ.get(
@@ -33,5 +32,3 @@ logging_config = {
 logging.config.dictConfig(logging_config)
 
 
-from .core import check, from_
-from .core.cli import cli_main

src/efi_conv/avportal/__init__.py
@@ -1,1 +1,1 @@
-from .avportal import efi_import, ISSUER_INFO
+from .avportal import ISSUER_INFO, efi_import

src/efi_conv/avportal/avportal.py
@@ -11,7 +11,6 @@ from ..core.utils import described_by_issuer
 from .generated.ntm_4_avefi import ntm_4_av_efi as ntm
 from .generated.ntm_4_avefi import ntm_4_av_efi_schema as ntm_main
 
-
 ROOT_CLASS = ntm_main.Resource
 log = logging.getLogger(__name__)
 parser = XmlParser()
@@ -52,7 +51,7 @@ def map_to_efi(input: ROOT_CLASS) -> list[efi.MovingImageRecord]:
                 f" productionYear={input.production_year},"
                 f" iwfProductionYear={input.iwf_production_year}")
     else:
-        log.warning(f"No production year")
+        log.warning("No production year")
     event = efi.ProductionEvent(has_date=production_year)
     work.has_event.append(event)
     producers = []

src/efi_conv/core/avefi.py
@@ -1,8 +1,8 @@
 import pathlib
-from typing import Annotated
 
 from avefi_schema import model_pydantic_v2 as efi
 from pydantic import ValidationError
+
 
 def load(source: pathlib.Path | str) -> list[efi.MovingImageRecord]:
     """Load AVefi records from file."""

src/efi_conv/core/check.py
@@ -5,19 +5,17 @@ import logging
 import pathlib
 import re
 import sys
-from typing import List
 
 import appdirs
 from avefi_schema import model_pydantic_v2 as efi
 import click
-from jsonschema.validators import validator_for
 from jsonschema.exceptions import best_match
+from jsonschema.validators import validator_for
 import requests
 
 from . import avefi
 from .cli import cli_main
 from .settings import settings
-
 
 log = logging.getLogger(__name__)
 SCHEMA_SOURCE = 'https://raw.githubusercontent.com/AV-EFI/av-efi-schema/main/project/jsonschema/avefi_schema/model.schema.json'
@@ -85,7 +83,7 @@ def get_schema_validator(update_schema=False):
 
 
 def pass_checks(
-        efi_records: List[efi.MovingImageRecord], schema_validator,
+        efi_records: list[efi.MovingImageRecord], schema_validator,
         remove_invalid=False) -> bool:
     """Check records against schema and additional rules.
 

src/efi_conv/core/cli.py
@@ -1,6 +1,5 @@
 import click
 
-
 # To be imported in each subcommand module
 
 @click.group()

src/efi_conv/core/from_.py
@@ -9,7 +9,6 @@ from . import avefi
 from .cli import cli_main
 from .utils import described_by_issuer
 
-
 log = logging.getLogger(__name__)
 
 
@@ -37,7 +36,7 @@ def import_file(
     result = importer.efi_import(input_file)
     for record in result:
         if not(record.has_identifier):
-            raise ValueError(f"has_identifier missing for some record(s)")
+            raise ValueError("has_identifier missing for some record(s)")
         described_by = described_by_issuer(record, importer.ISSUER_INFO)
         if not(described_by.has_source_key):
             log.warning(

src/efi_conv/core/settings.py
@@ -1,5 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_prefix=f"{__package__}_")
 

commit 1a0eb0be406551a62a9ae23dd56c202c5719b96c

Fix bug introduced in commit d2aedee breaking app startup

src/efi_conv/main.py
@@ -29,3 +29,6 @@ logging_config = {
     "disable_existing_loggers": False,
 }
 logging.config.dictConfig(logging_config)
+
+from .core import check, from_  # noqa: E402, F401
+from .core.cli import cli_main  # noqa: E402, F401